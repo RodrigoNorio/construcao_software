4701d19b9bdeea5551328118a9db03a83a1faeeb
Bruno Cesar Puli Dala Rosa <bcesar.g6@gmail.com>
2019-10-28 23:06:08 -0300

    Criação das telas de cadastro de Produto e Recurso. Implementação inicial dos botões de navegação para telas de cadastro das entidades.

--- app/src/main/java/com/example/trabalhocs/View/Fragments/FragmentVendas.java
+++ app/src/main/java/com/example/trabalhocs/View/Fragments/FragmentVendas.java
@@ -1,17 +1,29 @@
 package com.example.trabalhocs.View.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import com.example.trabalhocs.R;
+import com.example.trabalhocs.View.CadastrarProdutoActivity;
+import com.example.trabalhocs.View.CadastrarRecursoActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FragmentVendas extends Fragment {
+
+    @BindView(R.id.btn_cadastrar_produto)
+    AppCompatButton btnCadastrarProduto;
+
+    @BindView(R.id.btn_cadastrar_recurso)
+    AppCompatButton btnCadastrarRecurso;
 
     public FragmentVendas() {
         // Required empty public constructor
@@ -31,4 +43,15 @@ public class FragmentVendas extends Fragment {
         return view;
     }
 
+    @OnClick(R.id.btn_cadastrar_produto)
+    void clickBtnCadastrarProduto() {
+        Intent intent = new Intent(getContext(), CadastrarProdutoActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.btn_cadastrar_recurso)
+    void clickBtnCadastrarRecurso() {
+        Intent intent = new Intent(getContext(), CadastrarRecursoActivity.class);
+        startActivity(intent);
+    }
 }
