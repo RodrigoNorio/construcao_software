a581ea1c18a8b39cacb6af48b96c6f0c453518cc
Bruno Cesar <bcesar.g6@gmail.com>
2020-01-12 17:36:14 -0300

    Fix constantes de tipos de medidas

--- app/src/main/java/com/example/trabalhocs/Utils/Constants.java
+++ app/src/main/java/com/example/trabalhocs/Utils/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
 
     public static final int SELECAO_VAZIA = -1;
 
-    public static final int TIPO_MEDIDA_GRAMAS = 1;
-    public static final int TIPO_MEDIDA_MILILITRO  = 2;
-    public static final int TIPO_MEDIDA_UNIDADE = 3;
-    public static final int TIPO_MEDIDA_KILO = 4;
-    public static final int TIPO_MEDIDA_LITRO = 5;
+    public static final int TIPO_MEDIDA_GRAMAS = 0;
+    public static final int TIPO_MEDIDA_MILILITRO  = 1;
+    public static final int TIPO_MEDIDA_UNIDADE = 2;
+    public static final int TIPO_MEDIDA_KILO = 3;
+    public static final int TIPO_MEDIDA_LITRO = 4;
 
     //AdapterListaRecursos
     public static final int COMPRA_RECURSOS = 1;
